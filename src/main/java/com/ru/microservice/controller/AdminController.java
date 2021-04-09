7fabc80bd3f543c4a8f5fdd3510b20630f415a13
eugeneosis <fckngbld75@gmail.com>
2021-04-09 16:27:13 +0300

    Implemented rest-controllers for user/admin layers

--- src/main/java/com/ru/microservice/controller/AdminController.java
+++ src/main/java/com/ru/microservice/controller/AdminController.java
@@ -6,47 +6,49 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
-@Controller
-@RequestMapping("/admin")
+@RestController
 @AllArgsConstructor
 @Slf4j
 public class AdminController {
 
     private final UserService userService;
 
-    @GetMapping(value = "/home")
+    @GetMapping(value = "/admin/home")
     public ModelAndView adminPage() {
         ModelAndView modelAndView = new ModelAndView();
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        User user = userService.findUserByEmail(auth.getName());
-        modelAndView.addObject("userName", "Добро пожаловать " + user.getName() + " " + user.getLastName() + " (" + user.getEmail() + ")");
+        User admin = userService.findUserByEmail(auth.getName());
+        modelAndView.addObject("userName", "Добро пожаловать " + admin.getName() + " " + admin.getLastName() + " (" + admin.getEmail() + ")");
         modelAndView.addObject("adminMessage", "Контент доступен только для пользователей с ролью администратора");
         modelAndView.setViewName("admin/home");
-        log.info("login to admin profile {}", user);
+        log.info("login to admin profile {}", admin);
         return modelAndView;
     }
 
-    @GetMapping(value = "/users")
-    public String getAll(Model model) {
+    @GetMapping(value = "/admin/users")
+    public ModelAndView getAll(Model model) {
+        ModelAndView modelAndView = new ModelAndView();
         List<User> users = userService.getAll();
         model.addAttribute("users", users);
         log.info("getAll {}", users);
-        return "admin/list-users";
+        modelAndView.setViewName("admin/list-users");
+        return modelAndView;
     }
 
-    @GetMapping(value = "users/{id}")
-    public String delete(@PathVariable("id") Long id) {
+    @GetMapping(value = "/admin/users/{id}")
+    public ModelAndView delete(@PathVariable Long id) {
+        ModelAndView modelAndView = new ModelAndView();
         userService.deleteById(id);
-        log.info("delete {}", id);
-        return "redirect:/admin/users";
+        log.info("delete user with id {}", id);
+        modelAndView.setViewName("redirect:/admin/users");
+        return modelAndView;
     }
 }
--- src/main/java/com/ru/microservice/controller/UserController.java
+++ src/main/java/com/ru/microservice/controller/UserController.java
@@ -7,20 +7,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 
 @RestController
-@RequestMapping("/users")
 @AllArgsConstructor
 @Slf4j
 public class UserController {
 
     private final UserService userService;
 
-    @GetMapping(value = "/main")
+    @GetMapping(value = "/users/main")
     public ModelAndView userPage() {
         ModelAndView modelAndView = new ModelAndView();
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
