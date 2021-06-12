4e61d0c0195fa266c84b1b9f74a84136bbe80530
eugeneosis <fckngbld75@gmail.com>
2021-06-12 15:11:18 +0300

    Refactor: clean up rest template

--- src/main/java/com/ru/microservice/service/RestService.java
+++ src/main/java/com/ru/microservice/service/RestService.java
@@ -15,8 +15,8 @@ public class RestService {
 
     private final RestTemplate template;
 
-    private static final String UI_REST_SERVICE_URL = "http://ui-rest-service/api/messages";
-    private static final String UI_REST_WITHOUT_USER_SERVICE_URL = "http://ui-rest-service/api/messageswithoutuser";
+    private static final String UI_REST_SERVICE_URL = "http://ui-rest-service/api/raw/messages";
+    private static final String SORTED_DATA_UI_REST_SERVICE_URL = "http://ui-rest-service/api/messages";
     private static final String SERVICE_FALLBACK_CONSTANT = "Сервис временно не доступен. \n\nПопробуйте обновить страницу или зайти позднее.";
 
     @Retryable(maxAttempts = 5, value = RuntimeException.class, backoff = @Backoff(delay = 50, multiplier = 2))
@@ -26,12 +26,12 @@ public class RestService {
         return response;
     }
 
-//    @Retryable(maxAttempts = 5, value = RuntimeException.class, backoff = @Backoff(delay = 50, multiplier = 2))
-//    public String getAllMessagesWithoutUser() {
-//        String response = template.getForObject(UI_REST_WITHOUT_USER_SERVICE_URL, String.class);
-//        log.info(response);
-//        return response;
-//    }
+    @Retryable(maxAttempts = 5, value = RuntimeException.class, backoff = @Backoff(delay = 50, multiplier = 2))
+    public String getSortedMessages() {
+        String response = template.getForObject(SORTED_DATA_UI_REST_SERVICE_URL, String.class);
+        log.info(response);
+        return response;
+    }
 
     @Recover
     public String failed() {
